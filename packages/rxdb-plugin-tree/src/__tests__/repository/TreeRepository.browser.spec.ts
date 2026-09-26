7850007432ad3e56ea597b00aa4765dece76a72a
Jimmy Liu <hero63418@gmail.com>
2026-09-27 05:56:50 +1300

    222

--- packages/rxdb-plugin-tree/src/__tests__/repository/TreeRepository.browser.spec.ts
+++ packages/rxdb-plugin-tree/src/__tests__/repository/TreeRepository.browser.spec.ts
@@ -1,4 +1,4 @@
-import { ENTITY_STATIC_TYPES, RxDBError, SyncType, type RxDB, type UUID } from '@aiao/rxdb';
+import { createEntitySyncResolver, ENTITY_STATIC_TYPES, RxDBError, SyncType, type RxDB, type UUID } from '@aiao/rxdb';
 import { METADATA } from '@aiao/rxdb/testing';
 import { BehaviorSubject, NEVER, Observable, firstValueFrom, of } from 'rxjs';
 import { beforeEach, describe, expect, it, vi } from 'vitest';
@@ -67,6 +67,7 @@ describe('TreeRepository', () => {
     mockRxDB = {
       localAdapter$: of(localAdapter),
       remoteAdapter$: NEVER,
+      entitySync: createEntitySyncResolver(undefined),
       entityManager: {
         createEntityRef: vi.fn(),
         getEntityRef: vi.fn(),
